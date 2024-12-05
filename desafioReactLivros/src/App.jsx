0624b214c95c952eace69be7c73ed4a484a85296
BiaBicalho <beatrizbicalho2@gmail.com>
2024-12-04 21:49:07 -0300

    add folders with componenets header

---
+++ desafioReactLivros/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './Components/header/Header.jsx'
+import Footer from './Components/footer/Footer.jsx'
+import './globalStyle/globalStyle.scss'
+export default function App(){
+  return (
+    <section>
+      <Header/>
+      <Footer/>
+    </section>
+  )
+}
---
+++ desafioReactLivros/src/Components/header/Header.jsx
@@ -0,0 +1,33 @@
+import{BrowserRouter, Routes,Route, Link} from 'react-router-dom'
+import logoLivro from '../../assets/logoLivro.png'
+import lupa from '../../assets/lupa.png'
+import s from './header.module.scss'
+
+export default function Header(){
+    return(
+        <BrowserRouter>
+            <header>
+                <section className={s.logoHeader}>
+                    <img src={logoLivro} alt="" />
+                    <h1>Livros Vai na Web</h1>
+                </section>
+                <nav className={s.menuHeader}>
+                    <ul>
+                        <li><Link className={s.link} to='/'>Início</Link></li>
+                        <li><Link className={s.link} to='/livrosdoados'>Livros Doados</Link></li>
+                        <li><Link className={s.link} to='/querodoar'>Quero Doar</Link></li>
+                    </ul>
+                </nav>
+                <section className={s.barraBusca}>
+                    <input type="search" name="" id="" placeholder='O que está a procura?' />
+                    <button><img src={lupa} alt="" /></button>
+                </section>
+            </header>
+            <Routes>
+                <Route path='/' element={<Inicio/>}/>
+                <Route path='/livrosdados' element={<LivrosDoados/>}/>
+                <Route path='/querodoar' element={<QueroDoar/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
